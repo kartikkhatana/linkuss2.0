e42cc585f963863cf0388ca8385223fa0df5e8c0
Ayush-S-Rawat <ayushrawat772002@gmail.com>
2023-03-07 17:06:22 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreen(),
+      home: LoginPage(),
     );
   }
 }
